bfee9042d211cd5471027f13756755274a36bbbc
umutaltinsoy <umutcaltinsoy@gmail.com>
2022-01-18 02:08:29 +0300

    Yeni surum geldi

--- src/main/java/com/innova/a0/iocsiz/cdisiz/JavaManager.java
+++ src/main/java/com/innova/a0/iocsiz/cdisiz/JavaManager.java
@@ -3,7 +3,10 @@ package com.innova.a0.iocsiz.cdisiz;
 public class JavaManager {
 	public void newJavaVersion() {
 		JavaVersion javaVersion = new JavaVersion();
-		javaVersion.javaVersion();
+		// javaVersion.javaVersion("17");
+		javaVersion.javaVersion("18");
+		// javaVersion.javaVersion("19");
+		
 	}
 	
 }
--- src/main/java/com/innova/a0/iocsiz/cdisiz/JavaVersion.java
+++ src/main/java/com/innova/a0/iocsiz/cdisiz/JavaVersion.java
@@ -1,8 +1,19 @@
 package com.innova.a0.iocsiz.cdisiz;
 
 public class JavaVersion {
-	public void javaVersion() {
-		System.out.println("Java 17");
+	public void javaVersion(String version) {
+		if (version.equals("17")) {
+			System.out.println("Java 17");
+		} else if (version.equals("18")) {
+			System.out.println("Java 18");
+		} else if (version.equals("19")) {
+			System.out.println("Java 19");
+		} else {
+			System.out.println("Başka bir sürüm girdiniz.");
+		}
 	}
+	
+	// Yukarıdaki durumda kodumuz iyice karmaşık bir hal almaya başladı.(Spaghetti
+	// Code)
 	
 }
